9b89acd6275b1314954f852146dbecad8be95283
rayen <rayen.andolsi98@gmail.com>
2023-09-27 22:00:51 +0100

    aaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'countdown';
+export class AppComponent implements OnInit {
+  days: any;
+  hr: any;
+  min: any;
+  sc: any;
+
+  ngOnInit() {
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + 8); // Add 10 days to the current date
+
+    setInterval(() => {
+      const currentDate = new Date();
+      const timeLeft = targetDate.getTime() - currentDate.getTime();
+      this.days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+      this.hr = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.min = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      this.sc = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    }, 1000);
+  }
+
+
 }
